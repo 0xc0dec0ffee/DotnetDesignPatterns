8f9ad69439333fa7285f93c29b8a6b46c274368e
tingwei628 <sbvq6p199xe@gmail.com>
2023-06-10 15:19:38 +0800

    feat: import ExampleClassLibrary

---
+++ Controllers/ChapterController.cs
@@ -0,0 +1,23 @@
+﻿using ExampleClassLibrary.Chapter2;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetDesignPatterns.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChapterController: ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Ch2([FromServices]TaxEngine taxEngine)
+        {
+            taxEngine.Demo();
+            return Ok(taxEngine.GetOK());
+        }
+        [HttpPost]
+        public IActionResult Ch3([FromServices]TaxEngine taxEngine)
+        {
+            taxEngine.Demo();
+            return Ok(taxEngine.GetOK());
+        }
+    }
+}
